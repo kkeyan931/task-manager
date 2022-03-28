68ad4191eaa9f75d8d680ad0af35641221382ebc
karthikn <kkeyan931@gmail.com>
2022-03-28 19:06:50 +0530

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <h1>About page</h1>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "footer",
+};
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "About",
+  inheritAttrs: false,
+};
+</script>
